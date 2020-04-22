1458fbaf3c05505548846b2c301331b63211a477
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 10:14:25 -0600

    Add test for starting with no recipes

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -11,4 +11,10 @@ class CookBookTest < Minitest::Test
 
     assert_instance_of CookBook, cookbook
   end
+
+  def test_it_starts_with_no_recipes
+    cookbook = CookBook.new
+
+    assert_equal [], cookbook.recipes 
+  end
 end
