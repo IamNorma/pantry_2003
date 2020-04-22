1187f67afa01d6b5e48854eb58d4c4af6f43e478
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 09:27:39 -0600

    Add test for stock check

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -15,4 +15,14 @@ class PantryTest < Minitest::Test
 
     assert_equal ({}), pantry.stock
   end
+
+  def test_it_can_do_stock_check
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({
+      name: "Cheese",
+      unit: "oz",
+      calories: 50})
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
 end
