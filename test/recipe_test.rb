d7424212a3b419312d15533aa27356f74b315596
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 09:44:30 -0600

    Add test for ingredients_required

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -13,7 +13,12 @@ class RecipeTest < Minitest::Test
   def test_it_has_a_name
     recipe1 = Recipe.new("Mac and Cheese")
 
-    assert_equal "Mac and Cheese", recipe1.name 
+    assert_equal "Mac and Cheese", recipe1.name
   end
 
+  def test_it_starts_with_no_ingredients_required
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_equal ({}), recipe1.ingredients_required 
+  end
 end
