75c6aaed889e07b62be8fb755c9f58f00b5fe4b2
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 09:12:34 -0600

    Add attr_reader

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient
+  attr_reader :name,
+              :unit,
+              :calories
+
   def initialize(ingredient_params)
     @name = ingredient_params[:name]
     @unit = ingredient_params[:unit]
