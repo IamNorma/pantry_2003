d9afadcf0ab502dc7582ffb99395c2301b64ee6e
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 09:35:41 -0600

    Add ingredient2 to test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -22,10 +22,16 @@ class PantryTest < Minitest::Test
       name: "Cheese",
       unit: "oz",
       calories: 50})
+    ingredient2 = Ingredient.new({
+      name: "Macaroni",
+      unit: "oz",
+      calories: 200})
 
     assert_equal 0, pantry.stock_check(ingredient1)
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
     assert_equal 15, pantry.stock_check(ingredient1)
+    pantry.restock(ingredient2, 7)
+    assert_equal 7, pantry.stock_check(ingredient2)
   end
 end
