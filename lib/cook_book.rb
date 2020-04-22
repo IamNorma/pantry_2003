206676073570d148e65097851aa65a5b174a2abf
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 10:43:19 -0600

    Add ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -6,6 +6,14 @@ class CookBook
   end
 
   def add_recipe(recipe)
-    @recipes << recipe 
+    @recipes << recipe
+  end
+
+  def ingredients
+    @recipes.map do |recipe|
+    recipe.ingredients_required.map do |ingredient, amount_required|
+      ingredient.name
+    end
+    end.flatten.uniq
   end
 end
