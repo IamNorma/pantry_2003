2f91360b5ac4e42ef233d8dc0937c53db7647f5b
Norma Lopez <norma_l304@yahoo.com>
2020-04-22 09:52:31 -0600

    Expand test for ingredients_required by adding ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -18,7 +18,20 @@ class RecipeTest < Minitest::Test
 
   def test_it_starts_with_no_ingredients_required
     recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({
+      name: "Cheese",
+      unit: "oz",
+      calories: 50})
+    ingredient2 = Ingredient.new({
+      name: "Macaroni",
+      unit: "oz",
+      calories: 200})
+    expected = {ingredient1 => 6, ingredient2 => 8}
 
-    assert_equal ({}), recipe1.ingredients_required 
+    assert_equal ({}), recipe1.ingredients_required
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    assert_equal expected, recipe1.ingredients_required
   end
 end
